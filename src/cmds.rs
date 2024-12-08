24d8e05c258f48e6116efe4e6212b7e5e1f39c4b
mojidev-py <pycharmdudeig@gmail.com>
2024-12-07 21:43:53 -0800

    got cmds working

--- src/cmds.rs
+++ src/cmds.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::OpenOptions, io::{self, Error, Read, Write}};
+use std::{env, fs::OpenOptions, io::{self, Error, Read, Write}, process::exit};
 use supports_color;
 use colored::Colorize;
 use whoami;
@@ -15,17 +15,22 @@ pub(crate) fn running_loop() -> Result<(),Error> {
     }
     let mut input = String::new();
     print!("{}{} ~{}~ {}  ","fruitysh@".red(),whoami::username().red(),env::current_dir()?.to_str().unwrap(),">>".green());
+    io::stdout().flush().unwrap();
     match io::stdin().read_line(&mut input) {
         Ok(_) => route_to_cmd(&input).expect("yuh"),
         Err(err) => panic!("fruitysh had to exit since you did not provide valid input. :{err:#?}") 
     // allowing for shell to open without closing, repetitive, but might work
     }
-    while input != "quit" && input != "" {
+    input.clear();
+    while input != "quit" && input != " " {
+        
         print!("{}{} ~{}~ {}  ","fruitysh@".red(),whoami::username().red(),env::current_dir()?.to_str().unwrap(),">>".green());
+        io::stdout().flush().unwrap();
         match io::stdin().read_line(&mut input) {
-            Ok(_) => route_to_cmd(&input).expect("yuh"),
+            Ok(_) => route_to_cmd(&input).expect("Message for Quit:"),
             Err(err) => panic!("fruitysh had to exit since you did not provide valid input. :{err:#?}") 
         }
+        input.clear();
         
     }
     Ok(())
@@ -47,6 +52,8 @@ fn route_to_cmd(cmd: &str) -> Result<(), Error> {
     } else if cmd.contains("write") && !cmd.contains("|") {
         let arguments: Vec<&str> = cmd.split(" ").collect();
         tee(arguments[1],arguments[2])?;
+    } else if cmd.contains("quit") {
+        exit(0);
     } else {
         println!("{}: Command {} was not found.","[fruitysh]".green(),cmd.bold())
     }
@@ -55,32 +62,34 @@ fn route_to_cmd(cmd: &str) -> Result<(), Error> {
 }
 
 fn cat(path: &str) -> Result<(),Error> {
+    if path.trim_end() == "help" {
+        println!("{} \n view [PATH] \n Help page for view: \n view is a command that allows you to view the contents of a file, if it's encoded in utf-8, which is likely the case. \n view searches for files relative to the cwd.","[fruitysh@view]".green())
+    } else { 
     let mut output = String::new();
     let mut current_dir = env::current_dir()?.to_str().unwrap().to_owned();
-    current_dir.push_str(path);
+    current_dir.push_str("\\");
+    current_dir.push_str(path.trim_end());
     let mut pathfl = OpenOptions::new().read(true).open(current_dir)?;
     let metadata = pathfl.metadata()?.modified()?;
-    if path == "help" {
-        print!("{} \n view [PATH] \n Help page for view: \n view is a command that allows you to view the contents of a file, if it's encoded in utf-8, which is likely the case. \n view searches for files relative to the cwd.","[fruitysh@view]".green())
-    }
     pathfl.read_to_string(&mut output)?;
     print!("{} File modified at {:#?}. \n Contents: \n {}","[fruitysh@view]:".green(),metadata,&output);
+    }
     Ok(())
 }
 
 fn cd(path: &str) -> Result<(), Error> {
     let mut new_dir_path = env::current_dir()?.to_str().unwrap().to_owned();
-    if path == "help" {
-        print!("{} \n switchdir [RELPATH] \n Help page for view: \n switchdir is a command that allows you to navigate around your cwd, using relative paths.","[fruitysh@switchdir]:".green())
+    if path.trim_end() == "help" {
+        println!("{} \n switchdir [RELPATH] \n Help page for view: \n switchdir is a command that allows you to navigate around your cwd, using relative paths.","[fruitysh@switchdir]:".green())
     }
-    new_dir_path.push_str(path);
+    new_dir_path.push_str(path.trim_end());
     env::set_current_dir(new_dir_path)?;
     Ok(())
 }
 
 fn tee(path: &str,text: &str) -> Result<(),Error> {
-    if path == "help" {
-        print!("{} \n write [PATH] [TEXT] \n Help page for write: \n write is a command that allows you to write TEXT into a file at PATH.","[fruitysh@write]:".green())
+    if path.trim_end() == "help" {
+        println!("{} \n write [PATH] [TEXT] \n Help page for write: \n write is a command that allows you to write TEXT into a file at PATH.","[fruitysh@write]:".green())
     }
     let mut pathfl= OpenOptions::new().read(true).write(true).append(true).open(path)?;
     let buf = text.as_bytes();
