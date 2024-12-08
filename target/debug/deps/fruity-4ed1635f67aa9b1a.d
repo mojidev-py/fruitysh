0e3814a5ad17d41bf46b1cbd8bf8ccd4aa8de9d1
mojidev-py <pycharmdudeig@gmail.com>
2024-12-07 19:45:57 -0800

    added target

---
+++ target/debug/deps/fruity-4ed1635f67aa9b1a.d
@@ -0,0 +1,6 @@
+C:\Users\shrav\fruity\target\debug\deps\libfruity-4ed1635f67aa9b1a.rmeta: src/main.rs src\cmds.rs
+
+C:\Users\shrav\fruity\target\debug\deps\fruity-4ed1635f67aa9b1a.d: src/main.rs src\cmds.rs
+
+src/main.rs:
+src\cmds.rs:
---
+++ target/debug/deps/fruity-cad40a4c1c2f1eb6.d
@@ -0,0 +1,6 @@
+C:\Users\shrav\fruity\target\debug\deps\libfruity-cad40a4c1c2f1eb6.rmeta: src/main.rs src\cmds.rs
+
+C:\Users\shrav\fruity\target\debug\deps\fruity-cad40a4c1c2f1eb6.d: src/main.rs src\cmds.rs
+
+src/main.rs:
+src\cmds.rs:
